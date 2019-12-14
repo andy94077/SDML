689a27e5be10736d5bda08eb9b3bbcc7b806398c
andy94077 <andy94077@gmail.com>
2019-12-14 17:04:26 +0800

    Add svm option and additional training to dense_multi.py

--- final/dense_multi.py
+++ final/dense_multi.py
@@ -11,6 +11,7 @@ from tensorflow.keras import backend as K
 import tensorflow as tf
 
 import utils
+from train_2step import clf_svm
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '2' 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -23,6 +24,8 @@ parser.add_argument('model_path')
 parser.add_argument('-T', '--no-training', action='store_true')
 parser.add_argument('-s', '--submit')
 parser.add_argument('-p', '--predict')
+parser.add_argument('-t', '--additional-training', nargs=2)
+parser.add_argument('--svm', nargs='?', const='.svm', default=False)
 args = parser.parse_args()
 
 data_path = args.data_path
@@ -30,15 +33,20 @@ model_path = args.model_path
 training = not args.no_training
 submit = args.submit
 predict = args.predict
+additional_training = args.additional_training
+svm = model_path[:-3] + args.svm if args.svm else args.svm
 
 trainX, trainY = utils.load_train_data(data_path)#, drop_columns=['F2', 'F7', 'F12'])
 if predict:
     trainY = np.round(np.load(predict))
 else:
     trainY = trainY.astype(np.float32)
-label_smoothing = 0.2
+label_smoothing = 0.0
 trainY = trainY * (1 - label_smoothing) + label_smoothing / 2
 trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY)
+if additional_training:
+    trainX2, trainY2 = np.load(additional_training[0]), np.load(additional_training[1])
+    trainX, trainY = np.concatenate([trainX, trainX2], axis=0), np.concatenate([trainY, trainY2.ravel()], axis=0)
 missing_col, valid_missing_col = trainX[:, [1, 6, 11]], validX[:, [1, 6, 11]]
 trainX = np.delete(trainX, [1, 6, 11], axis=1)
 validX = np.delete(validX, [1, 6, 11], axis=1)
@@ -78,9 +86,9 @@ if os.path.exists(model_path):
 
 plot_model(model, 'model.jpg')
 if training:
-    checkpoint = ModelCheckpoint(model_path, 'val_class_acc', verbose=1, save_best_only=True, save_weights_only=True)
-    reduce_lr = ReduceLROnPlateau('val_class_acc', 0.5, 10, verbose=1, min_lr=1e-6)
-    early_stopping = EarlyStopping('val_class_acc', patience=50, restore_best_weights=True)
+    checkpoint = ModelCheckpoint(model_path, 'val_loss', verbose=1, save_best_only=True, save_weights_only=True)
+    reduce_lr = ReduceLROnPlateau('val_loss', 0.5, 10, verbose=1, min_lr=1e-6)
+    early_stopping = EarlyStopping('val_loss', patience=50, restore_best_weights=True)
     logger = CSVLogger(model_path+'.csv', append=True)
     tensorboard = TensorBoard(model_path[:model_path.rfind('.')]+'_logs', batch_size=1024, update_freq='epoch')
     model.fit(trainX, [trainY, missing_col[:, 0], missing_col[:, 1], missing_col[:, 2]], batch_size=512, epochs=500, validation_data=(validX, [validY, valid_missing_col[:, 0], valid_missing_col[:, 1], valid_missing_col[:, 2]]), verbose=2, callbacks=[checkpoint, reduce_lr, early_stopping, logger, tensorboard])
@@ -89,6 +97,15 @@ if submit:
     out = tf.cast(out*2, tf.int32)
     submit_model = Model(I, out)
     utils.submit(submit_model, submit)
+elif svm:
+    X, Y = utils.load_train_data(data_path)
+    Y = Y.astype(int)
+    trainX = np.delete(X, [1, 6, 11], axis=1)
+    _, f2, f7, f12 = model.predict(trainX, batch_size=1024)
+    X[:, 1] = f2.ravel()
+    X[:, 6] = f7.ravel()
+    X[:, 11] = f12.ravel()
+    clf_svm(X, Y, save_model=svm)
 else:
     trainX, trainY = utils.load_train_data(data_path)
     trainY = trainY.astype(int)

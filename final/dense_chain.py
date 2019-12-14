3a8842fcca9348fda84d497883174e482aa66fb6
andy94077 <andy94077@gmail.com>
2019-12-14 17:03:47 +0800

    Add svm option to dense_chain.py

--- final/dense_chain.py
+++ final/dense_chain.py
@@ -11,6 +11,7 @@ from tensorflow.keras import backend as K
 import tensorflow as tf
 
 import utils
+from train_2step import clf_svm
 
 def build_regression_model(input_shape, name=None):
     model = Sequential([
@@ -40,15 +41,19 @@ parser.add_argument('data_path')
 parser.add_argument('model_path')
 parser.add_argument('-T', '--no-training', action='store_true')
 parser.add_argument('-s', '--submit')
+parser.add_argument('--svm', nargs='?', const='.svm', default=False)
 args = parser.parse_args()
 
 data_path = args.data_path
 model_path = args.model_path
 training = not args.no_training
 submit = args.submit
+svm = model_path[:-3] + args.svm if args.svm else args.svm
 
 trainX, trainY = utils.load_train_data(data_path)#, drop_columns=['F2', 'F7', 'F12'])
-trainY = trainY.astype(int)
+trainY = trainY.astype(np.float32)
+label_smoothing = 0.1
+trainY = trainY * (1 - label_smoothing) + label_smoothing / 2
 trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY)
 missing_col, valid_missing_col = trainX[:, [1, 6, 11]], validX[:, [1, 6, 11]]
 trainX = np.delete(trainX, [1, 6, 11], axis=1)
@@ -81,7 +86,10 @@ x = Dropout(0.5)(x)
 out = Dense(1, activation='sigmoid', kernel_regularizer=l2(1e-3), name='class')(x)
 
 model = Model(I, [out, f2, f7, f12])
-model.compile(Adam(1e-3), loss=['binary_crossentropy', 'mse', 'mse', 'mse'], loss_weights=[1, 3, 3, 3], metrics=[['acc'], [], [], []])#, ['mse'], ['mse'], ['mse']])
+#model.compile(Adam(1e-3), loss=['binary_crossentropy', 'mse', 'mse', 'mse'], loss_weights=[1, 3, 3, 3], metrics=[['acc'], [], [], []])#, ['mse'], ['mse'], ['mse']])
+def acc(y_true, y_pred):
+    return K.round(y_true[:, 0]) == K.round(y_pred[:, 0])
+model.compile(Adam(1e-3), loss=['binary_crossentropy', 'mse', 'mse', 'mse'], loss_weights=[1, 3, 3, 3], metrics=[[acc], [], [], []])
 
 if os.path.exists(model_path): 
     model.load_weights(model_path)
@@ -93,12 +101,21 @@ if training:
     reduce_lr = ReduceLROnPlateau('val_loss', 0.5, 10, verbose=1, min_lr=1e-6)
     logger = CSVLogger(model_path+'.csv', append=True)
     tensorboard = TensorBoard(model_path[:model_path.rfind('.')]+'_logs', batch_size=1024, update_freq='epoch')
-    model.fit(trainX, [trainY, missing_col[:, 0], missing_col[:, 1], missing_col[:, 2]], batch_size=256, epochs=100, validation_data=(validX, [validY, valid_missing_col[:, 0], valid_missing_col[:, 1], valid_missing_col[:, 2]]), callbacks=[checkpoint, reduce_lr, logger, tensorboard])
+    model.fit(trainX, [trainY, missing_col[:, 0], missing_col[:, 1], missing_col[:, 2]], batch_size=256, epochs=100, validation_data=(validX, [validY, valid_missing_col[:, 0], valid_missing_col[:, 1], valid_missing_col[:, 2]]), verbose=2, callbacks=[checkpoint, reduce_lr, logger, tensorboard])
 
 if submit:
     out = tf.cast(out*2, tf.int32)
     submit_model = Model(I, out)
     utils.submit(submit_model, submit)
+elif svm:
+    X, Y = utils.load_train_data(data_path)
+    Y = Y.astype(int)
+    trainX = np.delete(X, [1, 6, 11], axis=1)
+    _, f2, f7, f12 = model.predict(trainX, batch_size=1024)
+    X[:, 1] = f2.ravel()
+    X[:, 6] = f7.ravel()
+    X[:, 11] = f12.ravel()
+    clf_svm(X, Y, save_model=svm)
 else:
     model.load_weights(model_path)
     print(f'\n\033[32;1mTraining score: {model.evaluate(trainX, [trainY, missing_col[:, 0], missing_col[:, 1], missing_col[:, 2]], verbose=0)}')

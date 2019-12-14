570c2ff5fc7417a7692f404efd70f5efd9e13ca6
andy94077 <andy94077@gmail.com>
2019-12-14 17:05:05 +0800

    Add additional training to dense_task2.py

--- final/dense_task2.py
+++ final/dense_task2.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 
 import utils
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '3' 
+os.environ['CUDA_VISIBLE_DEVICES'] = '2' 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
@@ -23,6 +23,7 @@ parser.add_argument('model_path')
 parser.add_argument('-T', '--no-training', action='store_true')
 parser.add_argument('-s', '--submit')
 parser.add_argument('-p', '--predict')
+parser.add_argument('-t', '--additional-training', nargs=2)
 args = parser.parse_args()
 
 data_path = args.data_path
@@ -30,10 +31,14 @@ model_path = args.model_path
 training = not args.no_training
 submit = args.submit
 predict = args.predict
+additional_training = args.additional_training
 
 trainX, trainY = utils.load_train_data(data_path)#, drop_columns=['F2', 'F7', 'F12'])
 trainY = trainY.astype(int)
 trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY)
+if additional_training:
+    trainX2, trainY2 = np.load(additional_training[0]), np.load(additional_training[1])
+    trainX, trainY = np.concatenate([trainX, trainX2], axis=0), np.concatenate([trainY, trainY2.ravel()], axis=0)
 print(f'\033[32;1mtrainX: {trainX.shape}, validX: {validX.shape}, trainY: {trainY.shape}, validY: {validY.shape}\033[0m')
 
 I = Input(trainX.shape[1:])
@@ -63,7 +68,7 @@ if training:
     reduce_lr = ReduceLROnPlateau('val_loss', 0.5, 10, verbose=1, min_lr=1e-6)
     logger = CSVLogger(model_path+'.csv', append=True)
     tensorboard = TensorBoard(model_path[:model_path.rfind('.')]+'_logs', batch_size=1024, update_freq='epoch')
-    model.fit(trainX, trainY, batch_size=128, epochs=50, validation_data=(validX, validY), callbacks=[checkpoint, reduce_lr, logger, tensorboard])
+    model.fit(trainX, trainY, batch_size=512, epochs=50, validation_data=(validX, validY), verbose=2, callbacks=[checkpoint, reduce_lr, logger, tensorboard])
 
 if submit:
     out = tf.cast(out*2, tf.int32)
@@ -76,5 +81,10 @@ elif predict:
         predict += '.npy'
     np.save(predict, predY)
 else:
+    if training:
+        model.load_weights(model_path)
+    trainX, trainY = utils.load_train_data(data_path)#, drop_columns=['F2', 'F7', 'F12'])
+    trainY = trainY.astype(int)
+    trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY)
     print(f'\n\033[32;1mTraining score: {model.evaluate(trainX, trainY, verbose=0)}')
     print(f'Validation Score: {model.evaluate(validX, validY, verbose=0)}\033[0m')
